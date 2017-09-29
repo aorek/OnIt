f181d7f1f0e237132f4507fa775544dfd10b7464
aorek <aquerorodriguez@gmail.com>
2017-09-29 19:57:46 +0200

    Add BL project

---
+++ OnIt.BusinesLogic/BaseBL.cs
@@ -0,0 +1,24 @@
+﻿using OnIt.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnIt.BusinesLogic
+{
+   public class BaseBL
+   {
+      internal OnItDbContext context;
+      internal DbConnection connection;
+      internal string connectionString;
+
+      public BaseBL(string connectionString)
+      {
+         this.connectionString = connectionString;
+         context = new OnItDbContext(connectionString);
+         //connection = context;
+      }
+   }
+}
