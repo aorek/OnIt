afc18fe4d45ee918a9bb3225adbc1f438505e9ee
aorek <aquerorodriguez@gmail.com>
2017-10-03 21:18:33 +0200

    Add ConnectionStringSingleton

---
+++ OnIt.Helper/ConnectionStringSingleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnIt.Helper
+{
+   public class ConnectionStringSingleton
+   {
+      private static ConnectionStringSingleton instance;
+
+      public string ConnectionString { get; set; }
+
+      public static ConnectionStringSingleton Instance
+      {
+         get
+         {
+            if (instance == null)
+               instance = new ConnectionStringSingleton();
+            return instance;
+         }
+         
+      }
+
+      public ConnectionStringSingleton()
+      {
+         ConnectionString = "name=OnitDbContext";
+      }
+   }
+}
--- OnIt.Model/Model/OnItDbContext.cs
+++ OnIt.Model/Model/OnItDbContext.cs
@@ -1,16 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using OnIt.Helper;
+using System.Collections.Generic;
 using System.Data.Entity;
 
 namespace OnIt.Model
 {
-   public static class ConnectionString
-   {
-      public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=OnIt.Model.OnItContext;Integrated Security=true";
-   }
-
    public class OnItDbContext : DbContext
    {
-      public OnItDbContext() : base(ConnectionString.connectionString)
+      public OnItDbContext() : base(ConnectionStringSingleton.Instance.ConnectionString)
       {
       }
 
