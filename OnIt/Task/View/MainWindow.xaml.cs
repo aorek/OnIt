1dabf7b5726daeeb4a8f0bf3e3390ba38e51a2bf
aorek <aquerorodriguez@gmail.com>
2017-12-07 20:16:09 +0100

    Change the listbox selection mode to multiple and add doubleclick event listbox items

--- OnIt/Task/View/MainWindow.xaml.cs
+++ OnIt/Task/View/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.ObjectModel;
 using System.Data.Common;
 using System.Windows;
+using System.Windows.Input;
 
 namespace OnIt.Task.View
 {
@@ -11,11 +12,18 @@ namespace OnIt.Task.View
    /// </summary>
    public partial class MainWindow : Window
    {
+      private MainWindowCommand command;
       public MainWindow()
       {
          InitializeComponent();
 
-         DataContext = new MainWindowCommand();
+         command = new MainWindowCommand();
+         DataContext = command;
+      }
+
+      public void OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+      {
+         command.EditTask(sender);
       }
    }
 }
