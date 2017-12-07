255f8fbf165ba4c445ac148741b55a3d1585826d
aorek <aquerorodriguez@gmail.com>
2017-12-07 20:37:58 +0100

    Create TasksSingleton to store data in memory to improve the performance

---
+++ OnIt.BusinessLogic/TasksSingleton.cs
@@ -0,0 +1,28 @@
+﻿using OnIt.Helper;
+using OnIt.Model;
+using OnIt.Model.Repository;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnIt.BusinessLogic
+{
+   public class TasksSingleton
+   {
+      public ObservableCollection<TaskModel> Tasks { get; set; }
+
+      private static TasksSingleton instance;
+      public static TasksSingleton Instance
+      {
+         get
+         {
+            if (instance == null)
+               instance = new TasksSingleton();
+            return instance;
+         }
+      }
+   }
+}
